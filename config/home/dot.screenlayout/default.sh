9c966be173d6428bb36aea0dcc615d50881a9b13
sfusco <sfusco@ricin.local>
2018-03-26 22:25:35 -0500

    saving custom arandr screen layout and defaulting to it on login

---
+++ config/home/dot.screenlayout/default.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI-2 --off --output HDMI-1 --mode 1920x1200 --pos 2560x416 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x1080 --rotate normal --output DP-2 --mode 2560x1080 --pos 0x0 --rotate normal
+feh --bg-fill --randomize ~/.wallpaper/*
