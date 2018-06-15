5c9980dc48fd38ea7235f26e15050ea3c3483ec5
sfusco <sfusco@ricin.local>
2018-06-14 22:42:45 -0500

    skipping notifications when charging

--- config/home/dot.config/systemd/user/check-battery.sh
+++ config/home/dot.config/systemd/user/check-battery.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+bat_stat=`cat /sys/class/power_supply/BAT0/status`
+
+if [ "$bat_stat" = "Charging" ]
+then
+  exit 0
+fi
+
 bat_level=`cat /sys/class/power_supply/BAT0/capacity`
 bat_status=`acpi -b`
 
